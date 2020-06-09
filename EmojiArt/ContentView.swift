3b2fe5b5f3345952bbe69a176b9472af961dd567
Philipp <pd95@users.noreply.github.com>
2020-06-09 08:40:50 +0200

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Philipp on 09.06.20.
+//  Copyright © 2020 Philipp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
